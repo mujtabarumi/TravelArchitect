af648885c463076a02f4d0f9fd3178685611b03d
Rumi <mujtaba.rumi1@gmail.com>
2020-02-14 01:47:13 +0600

    migration and seed done from admin

---
+++ admin/database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        $data = [
+            'name' => 'Jhon Travel',
+            'email' => 'travelAdmin@gmail.com',
+            'password' => Hash::make('worldtour'),
+            'type_id' => 1,
+            'status' => 1,
+        ];
+        User::create($data);
+
+
+    }
+}
